935345528504b4164ef09c9703705ab75896dca6
gioo007 <gioayman344@gmail.com>
2025-04-19 05:03:35 +0200

    uploading files

--- Attendee.java
+++
@@ -1,8 +0,0 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
-public class Attendee extends Account {
-    private String address;
-}
---
+++ src/Attendee.java
@@ -0,0 +1,8 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Attendee extends Account {
+    private String address;
+}
