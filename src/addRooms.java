8edcad7bca6cd1f2c0a4ae2855894f51b1daa705
gioo007 <gioayman344@gmail.com>
2025-04-21 16:10:11 +0200

    validation l add rooms

--- src/addRooms.java
+++ src/addRooms.java
@@ -6,11 +6,27 @@ public class addRooms implements IOoperations{
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter room ID: ");
-        int roomID = input.nextInt();
+        int roomID;
+        while(true) {
+                System.out.print("Enter room ID: ");
+                roomID = input.nextInt();
 
-        System.out.print("Enter room capacity: ");
-        int roomCapacity = input.nextInt();
+                if(roomID >= 1 && roomID <= 300){
+                    break;
+                }
+                System.out.println("Please enter a valid room ID.");
+            }
+
+        int roomCapacity;
+        while(true) {
+            System.out.print("Enter room capacity (50-100-150): ");
+            roomCapacity = input.nextInt();
+
+            if (roomCapacity == 50 || roomCapacity == 100 ||roomCapacity == 150){
+                break;
+            }
+            System.out.println("Invalid capacity.");
+        }
 
         System.out.print("Enter room price: ");
         Double price = input.nextDouble();
